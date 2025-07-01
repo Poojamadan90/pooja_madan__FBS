946a9ce0f7f10902d495f36c402e83f3b6171c92
Poojamadan90 <poojamadan1001@gmail.com>
2025-07-02 00:12:33 +0530

    Add files via upload

---
+++ Test 1/3rd que.py
@@ -0,0 +1,11 @@
+# program for distant conversion
+
+km = float(input("Enter distance in kilometers: "))
+
+# Convert to meters and centimeters
+meters = km * 1000
+centimeters = km * 100000
+
+# Display results
+print(f"Distance in meters: meters")
+print(f"Distance in centimeters:centimeters")
