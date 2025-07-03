bd6c99f4a2c13c4831c86f3b7e3ff7830f110304
Poojamadan90 <poojamadan1001@gmail.com>
2025-07-03 20:07:50 +0530

    Update Area of tringle and rectangle.py

---
+++ Assignment 2/Area of tringle and rectangle.py
@@ -0,0 +1,13 @@
+ #calculate the area of triangle and rectangle
+
+base = int(input("enter base of trangle:"))
+height = int(input("enter height of triangle:"))
+Area   = 0.5* base * height
+print("Area of triangle :", Area)
+
+
+length = int(input("Enter length:"))
+breadth = int(input("Enter breadth:"))
+
+Area = (length*breadth)
+print("Area of Rectangle:", Area)
